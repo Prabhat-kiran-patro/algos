4aef969300e47a31697cca590f2b059f02d81165
Prabhat <prabhatkp2005@gmail.com>
2025-06-23 22:22:55 +0530

    added algorithms

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,8 +1,9 @@
 import time
 
+# for functions that do not return anything
 def time_lapsed(func):
-    execution_time=0
     def wrapper(*args, **kwargs):
+        execution_time=0
         start_time=time.perf_counter_ns()
         func(*args, **kwargs)
         end_time = time.perf_counter_ns()
@@ -10,6 +11,16 @@ def time_lapsed(func):
         return execution_time
     return wrapper
 
+# for functions that return something
+def time_lapsed_re(func):
+    def wrapper(*args, **kwargs):
+        execution_time=0
+        start_time=time.perf_counter_ns()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter_ns()
+        execution_time = end_time - start_time
+        return execution_time, value
+    return wrapper
 
 class Sorting():
 
@@ -70,5 +81,45 @@ class Sorting():
                 if arr[j]<arr[mini]:
                     mini=j
             arr[i], arr[mini] = arr[mini], arr[i]
-            
 
+class Searching():
+    index = -1
+
+    @time_lapsed_re
+    def lin_search(arr, n):
+        for i in range(len(arr)):
+            if arr[i]==n:
+                return i
+        else:
+            return f"{n} not found!"
+    
+    def binary_search(self, arr, n, l, r):
+        if l>=r:
+            return
+        m=(l+r)//2
+        if arr[m]>n:
+            self.binary_search(arr, n, l, m-1)
+        elif arr[m]<n:
+            self.binary_search(arr, n, m+1, r)
+        else:
+            self.index=m
+            return
+    
+    @time_lapsed_re
+    def bin_search(self, arr, n):
+        self.binary_search(arr, n, 0, len(arr)-1)
+        return self.index
+
+class TowerOfHanoi():
+    def __init__(self):
+        self.steps_to_take = []
+    def steps(self, n, s='A', h='B', d='C'):
+        if n==0:
+            return
+        self.steps(n-1, s, d, h)
+        self.steps_to_take.append(s+" -> "+d)
+        self.steps(n-1, h, s, d)
+    @time_lapsed_re
+    def towofh(self, n):
+        self.steps(n)
+        return self.steps_to_take
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-from algorithms.algorithms import Sorting
+from algorithms.algorithms import Sorting, TowerOfHanoi, Searching
 
 app = Flask(__name__)
 
@@ -15,10 +15,11 @@ def algorithm(algorithm_name):
     formatted_time = None
     valid = None
     user_input = None
+    user_target = None
     if request.method == 'POST':
         # getting user input from form
         user_input = request.form.get('user_input').strip() # the param is name of the input tag
-
+        user_target = request.form.get('target_input')
         # applying algorithms
         try:
             if not user_input:
@@ -30,26 +31,42 @@ def algorithm(algorithm_name):
                 for i in range(len(ulist)):
                     ulist[i] = int(ulist[i])
                 time_taken = Sorting.bubble_sort(ulist)
-                result = ulist
+                result = [ulist]
             elif algorithm_name == 'selecsrt':
                 ulist = user_input.split()
                 for i in range(len(ulist)):
                     ulist[i] = int(ulist[i])
                 time_taken = Sorting.selection_sort(ulist)
-                result = ulist
+                result = [ulist]
             elif algorithm_name == 'insersrt':
                 ulist = user_input.split()
                 for i in range(len(ulist)):
                     ulist[i] = int(ulist[i])
                 time_taken = Sorting.insertion_sort(ulist)
-                result = ulist
+                result = [ulist]
             elif algorithm_name == 'mergesrt':
                 ulist = user_input.split()
                 for i in range(len(ulist)):
                     ulist[i] = int(ulist[i])
                 time_taken = Sorting.merge_sort(ulist)
-                result = ulist
-            
+                result = [ulist]
+            elif algorithm_name == 'towofh':
+                udisks = int(user_input)
+                inst = TowerOfHanoi()
+                time_taken, result = inst.towofh(udisks)
+            elif algorithm_name == 'binsrch':
+                ulist = user_input.split()
+                for i in range(len(ulist)):
+                    ulist[i] = int(ulist[i])
+                inst = Searching()
+                time_taken, result = inst.bin_search(ulist, int(user_target))
+                result=[result]
+            elif algorithm_name == 'linsrch':
+                ulist = user_input.split()
+                for i in range(len(ulist)):
+                    ulist[i] = int(ulist[i])
+                time_taken, result = Searching.lin_search(ulist, int(user_target))
+                result=[result]
 
             
             if time_taken:
@@ -66,7 +83,8 @@ def algorithm(algorithm_name):
         result=result,
         time_taken=formatted_time,
         uinput=user_input,
-        ipvalidity=valid
+        ipvalidity=valid,
+        user_target=user_target
     )
 
 
